8f150043b959c471a1308e84b3687c4b82160a66
James Davidson <jamesd3142@gmail.com>
2026-01-29 07:35:34 +0800

    Got libwtf built and linked to msquic

--- dev.sh
+++ dev.sh
@@ -2,3 +2,7 @@
 ~/.homebrew/Cellar/libmsquic/2.5.6
 
 cc -I/Users/jamesdavidson/.homebrew/Cellar/libmsquic/2.5.6/include -L/Users/jamesdavidson/.homebrew/Cellar/libmsquic/2.5.6/lib -lmsquic asdf.c
+
+rm -rfv build ; cmake -DMSQUIC_INCLUDE_DIRS=/Users/jamesdavidson/.homebrew/Cellar/libmsquic/2.5.6/include -DWTF_USE_EXTERNAL_MSQUIC=on -B build
+export LIBRARY_PATH=$LIBRARY_PATH:/Users/jamesdavidson/.homebrew/Cellar/libmsquic/2.5.6/lib
+cmake --build build
